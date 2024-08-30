721bce26e997425ba6c29557be4122a76d005749
antoineFabr <163823977+antoineFabr@users.noreply.github.com>
2024-08-30 15:55:51 +0200

    mis a jour du planning

---
+++ ShootMeUp/ShootMeUp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShootMeUp
+{
+    internal class Program
+    {
+
+        
+        public class Player
+        {
+            private int _x;
+            private int _y;
+            private int _vitesse;
+            public Player(int x, int y, int vitesse)
+            {
+                _x = x;
+                _y = y;
+                _vitesse = vitesse;
+            }
+
+
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
