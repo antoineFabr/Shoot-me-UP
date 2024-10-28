36b6edb8e7e8f2a3ef6e14e40a637a4116a07375
antoineFabr <163823977+antoineFabr@users.noreply.github.com>
2024-10-28 13:09:02 +0100

    ajout de la partie db

---
+++ elf/elf/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace elf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] elf;
+            foreach (int elfs in elf)
+            {
+
+            }
+        }
+    }
+}
