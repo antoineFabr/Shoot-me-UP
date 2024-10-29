44b58a6bb842900c297f50a21d26f0e8be85be5b
antoineFabr <163823977+antoineFabr@users.noreply.github.com>
2024-10-29 16:49:33 +0100

    doc ++

---
+++ ConsoleApp1/ConsoleApp1/Car.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Car
+    {
+        public int speed;
+        private string modele;
+        private string _color;
+        private Pasenger _pasenger;
+
+        public List<Pasenger> _pasengers;
+
+
+        public Car(string color)
+        {
+            _color = color;
+            _pasenger = new Pasenger(123,"Antoine");
+        }
+        public void AddPasengers(Pasenger nom)
+        {
+
+            _pasengers.Add(nom);
+        }
+        public void ShowPasengers()
+        {
+            foreach (Pasenger p in _pasengers)
+            {
+                Console.WriteLine(p);
+            }
+        }
+       
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class 
+    {
+        public string Name;
+        public int DriverLiscenceNum;
+
+        public Pasenger(int liscence, string name)
+        {
+            DriverLiscenceNum = liscence;
+            Name = name;
+        }
+
+
+
+    }
+}
